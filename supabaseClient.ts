6d0af8963c260d5a49e1f8da7697e81bd7f8dcc0
bayantai2012-star <bayantai2012@gmail.com>
2026-09-06 22:06:55 +0500

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://bvibbwairbpjghdjzyyd.supabase.co";
-
-const supabaseAnonKey = "sb_publishable_NAKcWrNDn9Ba0hnBUvMepA_NR70Aref";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
