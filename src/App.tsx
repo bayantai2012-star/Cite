572153ec3e76dec814a22ae49cfca4d298ea59be
bayantai2012-star <bayantai2012@gmail.com>
2026-09-07 22:14:23 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -252,7 +252,7 @@ export default function App() {
 
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange(async (_event: any, nextSession) => {
+    supabase.auth.onAuthStateChange(async (_event: any, nextSession: any) => {
       if (!mounted) return;
       setSession(nextSession);
       if (nextSession?.user) {
